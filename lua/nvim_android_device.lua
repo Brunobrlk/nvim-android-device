c0301d1b798d819e8a3439388018788b684711ef
Biplab Dutta <biplabdutta860@gmail.com>
2025-02-17 21:48:41 +0545

    add a function that shows attached android device name

---
+++ lua/nvim_android_device.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+local function is_flutter_project()
+	local cwd = vim.fn.getcwd()
+	return vim.fn.filereadable(cwd .. "/pubspec.yaml") == 1
+end
+
+function M.get_attached_device()
+	if not is_flutter_project() then
+		return ""
+	end
+
+	local handle = io.popen("adb devices -l")
+	if handle == nil then
+		return ""
+	end
+
+	local result = handle:read("*a")
+	handle:close()
+
+	if result and result ~= "" then
+		for line in result:gmatch("[^\r\n]+") do
+			local model = line:match("model:([%w%s_%-]+) device")
+			if model then
+				model = model:gsub("_", " ")
+				return "📱 " .. model
+			end
+		end
+	end
+
+	return "No device connected"
+end
+
+-- Function to register lualine component
+function M.android_device_name()
+	return M.get_attached_device()
+end
+
+return M
